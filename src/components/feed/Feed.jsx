4f5d18c8491a5174ab6a821e3ba250739be8bcd4
Vivek Nair <vivekmanju53@gmail.com>
2023-05-15 22:50:01 +0530

    added context

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -4,22 +4,23 @@ import Post from "../post/Post"
 import "./feed.css";
 import axios from "axios"
 
-export default function Feed() {
+export default function Feed({username}) {
   const [posts, setPosts] = useState([]);
+
   useEffect(()=>{
     const fetchPosts = async()=>{
-    const res = await axios.get("posts/timeline/6458ef0947655ffcb1efb52b");
-    setPosts(res.data)
+      const res = username ? await axios.get("/posts/profile/"+username) : await axios.get("/posts/timeline/6458ef0947655ffcb1efb52b");
+      setPosts(res.data)
+    }
+    fetchPosts()
+  }, [username])
 
-  }
-  fetchPosts()
-  }, [])
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
         {posts.map((p) => (
-          <Post key={p.id} post={p} />
+          <Post key={p._id} post={p} />
         ))}
       </div>
     </div>
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -2,10 +2,18 @@ import "./post.css";
 import { MoreVert } from "@material-ui/icons";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import {format} from "timeago.js";
+import {Link} from "react-router-dom";
+
+
+
+
 export default function Post({ post }) {
-  const [like,setLike] = useState(post.like)
+  const [like,setLike] = useState(post.likes.length)
   const [isLiked,setIsLiked] = useState(false)
   const [user, setUser] = useState({})
+
+
   const likeHandler =()=>{
     setLike(isLiked ? like-1 : like+1)
     setIsLiked(!isLiked)
@@ -15,12 +23,12 @@ export default function Post({ post }) {
 
   useEffect(()=>{
     const fetchUser = async()=>{
-    const res = await axios.get(`users/${post.userId}`);
+    const res = await axios.get(`/users?userId=${post.userId}`);
     setUser(res.data)
 
   }
   fetchUser()
-  }, [])
+  }, [post.userId])
 
 
   return (
@@ -28,17 +36,24 @@ export default function Post({ post }) {
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img
-              className="postProfileImg"
-              // src={PF+Users.filter((u) => u.id === post?.userId)[0].profilePicture}
-              src={PF+user.profilePicture}
-              alt=""
-            />
+            <Link to={`/profile/${user.username}`}>
+              <img
+                className="postProfileImg"
+                // src={PF+Users.filter((u) => u.id === post?.userId)[0].profilePicture}
+                src={
+                  user.profilePicture
+                    ? PF + user.profilePicture
+                    : PF + "person/noAvatar.png"
+                }
+                alt=""
+              />
+            </Link>
+
             <span className="postUsername">
               {/* {Users.filter((u) => u.id === post?.userId)[0].username} */}
               {user.username}
             </span>
-            <span className="postDate">{post.date}</span>
+            <span className="postDate">{format(post.createdAt)}</span>
           </div>
           <div className="postTopRight">
             <MoreVert />
@@ -46,12 +61,12 @@ export default function Post({ post }) {
         </div>
         <div className="postCenter">
           <span className="postText">{post?.desc}</span>
-          <img className="postImg" src={ PF+post.photo } alt="" />
+          <img className="postImg" src={ PF+post.img } alt="" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img className="likeIcon" src="assets/like.png" onClick={likeHandler} alt="" />
-            <img className="likeIcon" src="assets/heart.png" onClick={likeHandler} alt="" />
+            <img className="likeIcon" src={`${PF}like.png`} onClick={likeHandler} alt="" />
+            <img className="likeIcon" src={`${PF}heart.png`} onClick={likeHandler} alt="" />
             <span className="postLikeCounter">{like} people like it</span>
           </div>
           <div className="postBottomRight">
