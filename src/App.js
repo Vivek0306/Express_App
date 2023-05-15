d0f5a26604fbd72a723ee3f42b732b08f189d1bd
Vivek Nair <vivekmanju53@gmail.com>
2023-05-16 00:34:13 +0530

    routing fixed

--- src/App.js
+++ src/App.js
@@ -7,18 +7,24 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
+
 function App() {
+  const {user} = useContext(AuthContext);
+
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Home/>
+          {user ? <Home/> : <Register />}
         </Route>
         <Route path="/login">
-          <Login />  
+          {user ? <Redirect to="/" /> : <Login />}  
         </Route>
         <Route path="/register">
-          <Register/>
+          {user ? <Redirect to="/" /> : <Register />}  
         </Route>
         <Route path="/profile/:username">
           <Profile/>
