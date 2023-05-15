428eabf73ca6430b1960b71b1c97030a6aa421c5
Vivek Nair <vivekmanju53@gmail.com>
2023-05-15 23:24:45 +0530

    auth context call fix

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,13 +1,19 @@
-import { useRef } from "react";
+import { useContext, useRef } from "react";
 import "./login.css";
+import { loginCall } from "../../apiCalls";
+import { AuthContext } from "../../context/AuthContext";
+
 
 export default function Login() {
   const email = useRef();
   const password = useRef();
+  const {user, isFetching, error, dispatch} = useContext(AuthContext);
+
   const handleClick = (e)=>{
     e.preventDefault();
-    console.log(email.current.value)
+    loginCall({email:email.current.value, password:password.current.value}, dispatch)
   }
+  console.log(user)
   return (
     <div className="login">
       <div className="loginWrapper">
